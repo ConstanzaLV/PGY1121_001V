7fcf9d50df31728702eb6ba082d35c91d3e00f94
Constanza Pamela <cons.lopezv@duocuc.cl>
2023-05-27 00:53:20 +0000

    Funcion

---
+++ funcion.py
@@ -0,0 +1,12 @@
+def saludar():
+    print("Hola mejor seccion")
+
+#
+
+#print("la suma es ", {sumar_1()})
+
+def sumar(a, b):
+    suma = a + b
+    print(f"La suma de los numeros es: {suma}")
+
+sumar = (5, 6)
